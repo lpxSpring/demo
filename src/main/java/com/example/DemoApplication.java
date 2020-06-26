74812ce169a792a05c8bfa387ae557b6b5039fb3
lpxSpring <1042942449@qq.com>
2020-06-26 20:31:51 +0800

    github代码提交测试

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	// GitHub代码提交测试
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
