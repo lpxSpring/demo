df7c5c9a2e5aa3159e8fa8a3db9dbee54a45d180
lpxSpring <1042942449@qq.com>
2020-06-26 20:35:08 +0800

    test

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
+		// GitHub代码提交测试
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
